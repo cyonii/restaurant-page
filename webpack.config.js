dcf32316e40bdc882251e9fc14a15456d62b7d6f
CY Kalu <cy23@tutanota.com>
2021-03-17 02:32:29 +0900

    Update packages
    add necessary packages and update webpack config so that it can build 
    fully from src

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,47 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
-    path: path.join(__dirname, 'dist')
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist'),
+    clean: true,
   },
   mode: 'development',
   devtool: 'source-map',
+  devServer: {
+    contentBase: './dist',
+  },
   module: {
-    rules: [
+    rules: [{
+        test: /\.(css|sass|scss)$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
+        test: /\.html$/i,
+        use: [{
+          loader: 'html-loader'
+        }]
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'images/',
+            publicPath: 'images/',
+          }
+        }]
       },
     ]
-  }
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
 }
