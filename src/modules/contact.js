3cb0e31531ed64083775f339c12a53ef88229e90
CY Kalu <cy23@tutanota.com>
2021-03-17 21:38:19 +0900

    Update contact pane

--- src/modules/contact.js
+++ src/modules/contact.js
@@ -6,12 +6,18 @@ function contactPage() {
   contactInfoWrapper.classList.add('contact-info');
 
   [
-    '#89b Forest Hill Drive, Loopside, PL 09098',
-    'Mon-Thurs:8am-8pm, Fri-Sun:8am-11pm'
+    ['Location', '#89b Forest Hill Drive, Loopside, PL 09098'],
+    ['Opening Hours', 'Mon-Thurs:8am-8pm, Fri-Sun:8am-11pm']
   ].forEach((item) => {
-    const line = document.createElement('p')
-    line.innerText = item;
-    contactInfoWrapper.appendChild(line);
+    const row = document.createElement('p');
+    const label = document.createElement('span');
+    const data = document.createTextNode(item[1]);
+
+    label.classList.add('label');
+    label.innerText = item[0];
+    row.appendChild(label);
+    row.appendChild(data);
+    contactInfoWrapper.appendChild(row);
   });
 
   return contactInfoWrapper;
